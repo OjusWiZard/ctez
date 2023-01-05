6fc4326558bf517bee43fc7b95f8959046485be8
Tarun Sharma <sharmatarun1019@gmail.com>
2023-01-05 20:21:30 +0530

    fix: added price impact calculations

--- frontend/app/src/components/Trade/Swap/Swap.tsx
+++ frontend/app/src/components/Trade/Swap/Swap.tsx
@@ -210,54 +210,45 @@ const Swap: React.FC = () => {
     if (cfmmStorage && values.amount && (target>0)) {
       console.log(cfmmStorage,baseStats)
       const { cashPool: tokenPool, tezPool: cashPool } = cfmmStorage;
+      // const cashPool = 329222887605 ;
+      // const tokenPool = 145510772615 ;
       const invariant = Number(cashPool) * Number(tokenPool);
-      let initialPrice: number;
+      // let initialPrice: number;
       const SwapAmount = values.amount * 1e6;
-      let recievedPrice: number;
+      // let recievedPrice: number;
       const cashSold = values.amount * 1e6;
       let  tokWithoutSlippage = 0
       if (formType === FORM_TYPE.TEZ_CTEZ){
         // console.log(cashPool.toNumber(),tokenPool.toNumber() , cashSold , target , "swap math");
-        tokWithoutSlippage = trade_dtez_for_dcash({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: cashSold , target , rounds: 4});
-        console.log()
+        // tokWithoutSlippage = trade_dtez_for_dcash({tez:cashPool , cash:tokenPool , dtez: SwapAmount , target , rounds: 4});
+        tokWithoutSlippage = trade_dtez_for_dcash({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: SwapAmount , target , rounds: 4});
       }
       else {
-        tokWithoutSlippage = trade_dcash_for_dtez({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: cashSold , target , rounds: 4});
-        console.log("here",tokWithoutSlippage)
+        tokWithoutSlippage = trade_dcash_for_dtez({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: SwapAmount , target , rounds: 4});
       }
       tokWithoutSlippage = (tokWithoutSlippage * 9999 / 10000)/1e6
-      // if (formType === FORM_TYPE.CTEZ_TEZ) { // ctez to tez
-      //   // 1 ctez = 11 tez
-      //   initialPrice = Number(cashPool) / Number(tokenPool);
-      //   const newTokenPool = Number(tokenPool) + SwapAmount * 0.9999;
-      //   const newCashPool = invariant / newTokenPool;
-      //   const difference = Number(cashPool) - newCashPool;
-      //   recievedPrice = difference / SwapAmount;
-      // } else { // tez to ctez
-      //   initialPrice = Number(tokenPool) / Number(cashPool);
-      //   const newCashPool = Number(cashPool) + SwapAmount * 0.9999;
-      //   const newTokenPool = invariant / newCashPool;
-      //   const difference = Number(tokenPool) - newTokenPool;
-      //   recievedPrice = difference / SwapAmount;
-      // }
+    
+      // let priceImpact = tokenOut.minus(nextTokenOut).dividedBy(tokenOut);
+      // priceImpact = priceImpact.multipliedBy(100);
+      // priceImpact = new BigNumber(Math.abs(Number(priceImpact)));
+      let newPrice = 1
       if (formType === FORM_TYPE.CTEZ_TEZ) { // ctez to tez
-        // 1 ctez = 11 tez
-        initialPrice = Number(cashPool) / Number(tokenPool);
-        const newTokenPool = Number(tokenPool) + SwapAmount * 0.9999;
         const newCashPool = Number(cashPool) - tokWithoutSlippage*1e6;
-        const difference = Number(cashPool) - newCashPool;
-        recievedPrice = difference / SwapAmount;
-        // console.log(Number(tokenPool), tokWithoutSlippage*1e6,difference, recievedPrice, "math");
+        const newTokenPool = Number(tokenPool) + SwapAmount * 0.9999;
+        console.log(Number(cashPool), newCashPool, Number(tokenPool),newTokenPool,"check ctez to tez")
+        newPrice = (trade_dcash_for_dtez({tez:newCashPool , cash:newTokenPool , dtez: SwapAmount , target , rounds: 4})*9999/10000)/1e6
+
       } else { // tez to ctez
-        initialPrice = Number(tokenPool) / Number(cashPool);
         const newCashPool = Number(cashPool) + SwapAmount * 0.9999;
         const newTokenPool = Number(tokenPool) - tokWithoutSlippage*1e6;
-        const difference = Number(tokenPool) - newTokenPool;
-        recievedPrice = difference / SwapAmount;
+        console.log(Number(cashPool), newCashPool, Number(tokenPool),newTokenPool,"check tez to ctez")
+        newPrice = (trade_dtez_for_dcash({tez:newCashPool , cash:newTokenPool , dtez: SwapAmount , target , rounds: 4})*9999/10000)/1e6
+
         // console.log(Number(tokenPool), tokWithoutSlippage*1e6,difference, recievedPrice, "math");
       }
-      const priceImpact1 = ((initialPrice - recievedPrice) * 100) / initialPrice;
-      setpriceImpact(priceImpact1);
+      const priceImpact1 = ((tokWithoutSlippage - newPrice) * 100) / tokWithoutSlippage;
+      console.log(tokWithoutSlippage,newPrice,priceImpact,"calc");
+      setpriceImpact(Math.abs(priceImpact1));
       setMinBuyValue(formatNumberStandard(tokWithoutSlippage.toFixed(6)));
       const minRece = tokWithoutSlippage - (tokWithoutSlippage * slippage) / 100;
       setMinReceived(minRece);
@@ -273,12 +264,8 @@ const Swap: React.FC = () => {
     let e_rate = 1;
     if(cfmmStorage){
       const { cashPool: tokenPool, tezPool: cashPool } = cfmmStorage;
-      // console.log(cashPool.toNumber(),tokenPool.toNumber() , 1000000 , target , "rate math");
     formType === FORM_TYPE.TEZ_CTEZ
-    ? 
-    
-    e_rate = (trade_dtez_for_dcash({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: 1000000 , target , rounds: 4})*9999/10000)/1e6
-    // formatNumberStandard(baseStats?.currentPrice ?? 1)
+    ? e_rate = (trade_dtez_for_dcash({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: 1000000 , target , rounds: 4})*9999/10000)/1e6
     : e_rate = (trade_dcash_for_dtez({tez:cashPool.toNumber() , cash:tokenPool.toNumber() , dtez: 1000000 , target , rounds: 4})*9999/10000)/1e6
 
     }
