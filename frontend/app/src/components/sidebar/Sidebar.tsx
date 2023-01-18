54a5342927c3116bbe82a995dd2fdf6f2d18c9f3
Tarun Sharma <sharmatarun1019@gmail.com>
2023-01-18 12:29:04 +0530

    fix: used util component on sidebar

--- frontend/app/src/components/sidebar/Sidebar.tsx
+++ frontend/app/src/components/sidebar/Sidebar.tsx
@@ -22,6 +22,7 @@ import { MODAL_NAMES } from '../../constants/modals';
 import { useCfmmStorage, useCtezBaseStats } from '../../api/queries';
 import { useThemeColors } from '../../hooks/utilHooks';
 import { formatNumberStandard } from '../../utils/numbers';
+import { trade_dtez_for_dcash } from '../../utils/swapUtils';
 
 export interface Props {
   handleCollapsed: React.MouseEventHandler;
@@ -47,51 +48,6 @@ const Sidebar: React.FC<Props> = ({ handleCollapsed, handleToggled, collapsed, t
   };
   const [rate,setRate] = useState(1);
   const { data: cfmmStorage } = useCfmmStorage();
-  const util = (x : number, y : number) =>{
-    const plus = x + y;
-    const minus = x - y ;
-    const plus_2 = plus * plus;
-    const plus_4 = plus_2 * plus_2;
-    const plus_8 = plus_4 * plus_4;
-    const plus_7 = plus_4 * plus_2 * plus;
-    const minus_2 = minus * minus;
-    const minus_4 = minus_2 * minus_2;
-    const minus_8 = minus_4 * minus_4;
-    const minus_7 = minus_4 * minus_2 * minus;
-    return [Math.abs(plus_8 - minus_8), 8 * (Math.abs(minus_7 + plus_7))] // 8n to 8
-}
-
-const newton = (p:any):number => {
-  if (p.n === 0) {
-      return p.dy
-  }
-      const util_arr =  util((p.x + p.dx),(Math.abs(p.y - p.dy)));
-      const new_u = util_arr[0];
-      const new_du_dy = util_arr[1];
-      const dy = p.dy + Math.abs((new_u - p.u) / new_du_dy);
-      return newton({x:p.x , y:p.y ,dx: p.dx , dy ,u: p.u ,n: p.n - 1});
-  
-}
-
-const newton_dx_to_dy =(
-  args :any
-) => {
-  const xp = args.x + args.dx ;
-  const u = util(args.x,args.y)[0] ;
-  return newton({x:args.x , y:args.y, dx:args.dx , dy:0 , u  ,n:args.rounds})
-}
-
-
-const trade_dtez_for_dcash = (
-  args : any
-) =>{
-  const x = args.tez * (2** 48); 
-  const y = args.target * args.cash;
-  const dx = args.dtez * (2** 48); 
-  const dy_approx = newton_dx_to_dy({x, y, dx, rounds:args.rounds});
-  const dcash_approx = dy_approx / args.target;
-  return dcash_approx
-}
 
 const rateCalc = (): number =>
 {
